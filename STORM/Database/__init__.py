6095b0b3e20ae97936c248852ed6acd9ff7b5f83
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-28 13:59:45 +0530

    Update __init__.py

--- STORM/Database/__init__.py
+++ STORM/Database/__init__.py
@@ -1,5 +1,5 @@
 from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-MONGO_URI = "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb+srv://riyihe1925:mIroEVXcVCGGwAFK@cluster0.g6ojimu.mongodb.net/?retryWrites=true&w=majority"
 mongo = MongoClient(MONGO_URI)
 db = mongo["Client"]
 
