836a206c16b7766064514cebbade8b8198df55b1
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-29 14:42:45 +0530

    ꜱᴛᴏʀᴍ 2.0

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 OPENAIKEY = getenv("OPENAIKEY")
-MURL = getenv("MANGO_URL")
 PM_PIC = "https://graph.org/file/bf1fdd404a82d508a7ed5.jpg"
 SESSION2 = getenv("SESSION2")
 SESSION3 = getenv("SESSION3")
