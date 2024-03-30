cbb8fc9286c6186157c1102067dabec5ce5b45f8
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-03-30 18:12:08 +0530

    Update sdimage.py

--- STORM/modules/sdimage.py
+++ STORM/modules/sdimage.py
@@ -1,22 +1,22 @@
 import os
 from pyrogram import Client, filters
 
-# Initialize Pyrogram Client
-app = Client("sdestructimage_downloader")
+trigger_words = ["😋🥰", "op", "wow", "super", "😋😍"]
 
-# Define the filter for the commands
-@Client.on_message(filters.command(["😋🥰", "op", "wow", "super", "😋😍"]) & filters.private & filters.reply)
-async def download_self_destruct_image(client, message):
+@filters.private & filters.me
+async def private_message_filter(_, __, message):
+    return message
+
+@Client.on_message(filters.text & private_message_filter)
+async def self_media_handler(client, message):
     try:
-        replied_message = message.reply_to_message
-        if not replied_message or not (replied_message.photo or replied_message.video):
+        replied = message.reply_to_message
+        if not replied:
             return
-        # Download the media file
-        file_path = await client.download_media(replied_message)
-        # Send the downloaded media file as a document
-        await client.send_document("me", file_path)
-        # Remove the downloaded media file from the local storage
-        os.remove(file_path)
+        if not (replied.photo or replied.video):
+            return
+        location = await client.download_media(replied)
+        await client.send_document("me", location)
+        os.remove(location)
     except Exception as e:
         print(f"Error: {e}")
-        return
