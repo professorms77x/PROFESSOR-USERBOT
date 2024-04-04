b8f50d2b20f4e5771f744f6a4b38e222f5129534
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-04 16:43:28 +0530

    Update lyrics.py

--- STORM/modules/lyrics.py
+++ STORM/modules/lyrics.py
@@ -2,9 +2,20 @@ import asyncio
 import sys
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from lyricsdb import search_lyrics
 from config import SUDO_USERS
+import lyricsgenius
 
+genius = lyricsgenius.Genius("jPnYlXn0YEF4xJLJJJ0V2fngWTmyK4c9scfIxFMpofO4-aKIWJ8t9f_11oCeZCLj")
+async def search_lyrics(song_title, artist_name):
+    try:
+        song = genius.search_song(song_title, artist_name)
+        if song:
+            return song.lyrics
+        else:
+            return f"ʟʏʀɪᴄꜱ ꜰᴏʀ '{song_title}' ʙʏ {artist_name} ɴᴏᴛ ꜰᴏᴜɴᴅ..."
+    except Exception as e:
+        return f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ: {str(e)}"
+        
 @Client.on_message(filters.command(["l", "lyrics"], ".") & filters.me | filters.user(SUDO_USERS))
 async def handle_lyrics_command(client: Client, message: Message):
     try:
