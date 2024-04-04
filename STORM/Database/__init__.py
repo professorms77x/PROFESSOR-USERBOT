99c945d0084f5d3ce742b9d1eec66d011bd454b5
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-04 10:15:59 +0530

    Delete STORM/Database/__init__.py

--- STORM/Database/__init__.py
+++
@@ -1,7 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
-
-mongo = MongoClient(mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority)
-
-db = mongo.Client
-
-## THIS MANGO DB FEATURE IS ONLY FOR LIMITED TIME SO MUST UPDATE THE REPO WHEN THIS FILE IS REMOVED
