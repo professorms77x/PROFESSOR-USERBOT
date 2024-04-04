20a45d98dc9a083d0af8485e56d5560d02452354
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-04 15:17:33 +0530

    Update lyricsdb.py

--- STORM/modules/lyricsdb.py
+++ STORM/modules/lyricsdb.py
@@ -1,14 +1,12 @@
 import lyricsgenius
 genius = lyricsgenius.Genius("jPnYlXn0YEF4xJLJJJ0V2fngWTmyK4c9scfIxFMpofO4-aKIWJ8t9f_11oCeZCLj")
-
 async def search_lyrics(song_title, artist_name):
     try:
-        # Search for the lyrics
         song = genius.search_song(song_title, artist_name)
         if song:
             return song.lyrics
         else:
-            return f"Lyrics for '{song_title}' by {artist_name} not found."
+            return f"ʟʏʀɪᴄꜱ ꜰᴏʀ '{song_title}' ʙʏ {artist_name} ɴᴏᴛ ꜰᴏᴜɴᴅ..."
     except Exception as e:
-        return f"An error occurred: {str(e)}"
+        return f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ: {str(e)}"
 
